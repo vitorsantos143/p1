4f3dcf6452dbda89a3c392782e3f59131943478a
Vitor Santos <vitor.santos143@aluno.cps.sp.gov.br>
2026-04-08 16:30:58 -0300

    commit att1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace att1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            mensageiro msg = new mensageiro();
+            msg.mensagem();
+        }
+    }
+}
